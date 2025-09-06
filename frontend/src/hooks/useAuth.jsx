82f3dcaeba56fe4b2abd913a65a45bc14097a4a8
mant0597 <123318369+mant0597@users.noreply.github.com>
2025-09-06 19:04:44 +0530

    updated node version

--- frontend/src/hooks/useAuth.jsx
+++ frontend/src/hooks/useAuth.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../context/AuthContext.jsx";
 
 const useAuth = () => useContext(AuthContext);
 
